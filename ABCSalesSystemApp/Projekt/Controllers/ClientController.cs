b38e691319a35561bb9a23dded2b30e65a9707dc
PC <s27000@pjwstk.edu.pl>
2024-06-24 14:26:08 +0200

    project init

---
+++ ABCSalesSystemApp/Projekt/Controllers/ClientController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Projekt.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+        [HttpPost("clients/firm/{krs:int}")]
+        public void AddFirm(int krs)
+        {
+
+        }
+
+        [HttpPost("clients/person/{pesel:int}")]
+        public void AddClient(int pesel)
+        {
+
+        }
+    }
+}
